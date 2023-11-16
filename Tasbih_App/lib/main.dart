dfb2384604b12d212fc97a1f59b46cddba5b9924
Mereke Sabitov <merekesabitov@gmail.com>
2023-11-16 15:38:51 +0500

    HomePage UI (update)

--- Tasbih_App/lib/main.dart
+++ Tasbih_App/lib/main.dart
@@ -28,21 +28,93 @@ class HomePage extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 15),
           child: Column(
             children: [
-              Container(
-                  height: 38,
-                  color: Colors.white,
-                  margin: const EdgeInsets.only(bottom: 20,)
-              ),
-              Container(
-                height: 260,
-                color: Colors.white,
-                margin: const EdgeInsets.only(bottom: 15),
-              ),
+              const TopPanel(),
+              const CounterPanel(),
               Expanded(child: Container(height: 50, color: Colors.white)),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class TopPanel extends StatelessWidget {
+  const TopPanel({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(bottom: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.white,
+                height: 38,
+                width: 276,
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              height: 38,
+              width: 54,
+              margin: const EdgeInsets.only(left: 20),
+            )
+          ],
+        ),
+    );
+  }
+}
+
+class CounterPanel extends StatelessWidget {
+  const CounterPanel({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 15),
+      child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 14),
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(vertical: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    height: 35,
+                    width: 35,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    height: 154,
+                    width: 154,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    height: 35,
+                    width: 35,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              height: 45,
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: const Text('Save Dhikr'),
+            ),
+          ],
+        ),
+      );
   }
 }
