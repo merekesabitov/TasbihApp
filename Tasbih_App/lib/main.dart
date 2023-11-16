58328f2f4dbb4f1def9dc14854097d3e529f4624
Mereke Sabitov <merekesabitov@gmail.com>
2023-11-17 02:07:35 +0500

    HomePage UI (updated)

--- Tasbih_App/lib/main.dart
+++ Tasbih_App/lib/main.dart
@@ -22,15 +22,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepOrange.shade50,
-      body: SafeArea(
+      body: const SafeArea(
         bottom: false,
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
+          padding: EdgeInsets.symmetric(horizontal: 15),
           child: Column(
             children: [
-              const TopPanel(),
-              const CounterPanel(),
-              Expanded(child: Container(height: 50, color: Colors.white)),
+              TopPanel(),
+              CounterPanel(),
+              DBPanel(),
             ],
           ),
         ),
@@ -40,14 +40,12 @@ class HomePage extends StatelessWidget {
 }
 
 class TopPanel extends StatelessWidget {
-  const TopPanel({
-    super.key,
-  });
+  const TopPanel({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.only(bottom: 20),
+        margin: const EdgeInsets.only(top: 10, bottom: 20),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -71,9 +69,7 @@ class TopPanel extends StatelessWidget {
 }
 
 class CounterPanel extends StatelessWidget {
-  const CounterPanel({
-    super.key,
-  });
+  const CounterPanel({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -82,7 +78,7 @@ class CounterPanel extends StatelessWidget {
       child: Column(
           children: [
             Container(
-              margin: EdgeInsets.only(bottom: 14),
+              margin: const EdgeInsets.only(bottom: 14),
               color: Colors.white,
               padding: const EdgeInsets.symmetric(vertical: 24),
               child: Row(
@@ -116,5 +112,57 @@ class CounterPanel extends StatelessWidget {
           ],
         ),
       );
+  }
+}
+
+class DBPanel extends StatelessWidget {
+  const DBPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: Container(
+      height: 50,
+      width: double.infinity,
+      color: Colors.white,
+      padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Last Saved Dhikrs',
+            style: TextStyle(fontWeight: FontWeight.bold,
+          ),
+          ),
+          Container(
+            width: 60,
+            height: 2,
+            margin: const EdgeInsets.only(top: 5, bottom: 25),
+            color: Colors.blue,
+          ),
+          Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 49,
+                margin: const EdgeInsets.only(bottom: 10),
+                color: Colors.blue,
+              ),
+              Container(
+                width: double.infinity,
+                height: 49,
+                margin: const EdgeInsets.only(bottom: 10),
+                color: Colors.blue,
+              ),
+              Container(
+                width: double.infinity,
+                height: 49,
+                margin: const EdgeInsets.only(bottom: 10),
+                color: Colors.blue,
+              ),
+            ],
+          )
+        ],
+      ),
+      ));
   }
 }
