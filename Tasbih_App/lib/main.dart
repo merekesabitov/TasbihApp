0c9bd27799a963bd53f17cb69f13c5c11331ab47
Mereke Sabitov <merekesabitov@gmail.com>
2023-11-22 15:46:45 +0500

    HomePage UI (updated database panel)

--- Tasbih_App/lib/main.dart
+++ Tasbih_App/lib/main.dart
@@ -26,7 +26,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepOrange.shade50,
+      backgroundColor: Colors.brown.shade50,
       body: const SafeArea(
         bottom: false,
         child: Padding(
@@ -210,9 +210,14 @@ class DBPanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(child: Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10)),
+      ),
       height: 50,
       width: double.infinity,
-      color: Colors.white,
       padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -229,28 +234,60 @@ class DBPanel extends StatelessWidget {
             margin: const EdgeInsets.only(top: 5, bottom: 25),
             color: myBlue,
           ),
-          Column(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 49,
+          Expanded(
+            child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF9F9F9),
+                    borderRadius: BorderRadius.circular(10)
+                  ),
                 margin: const EdgeInsets.only(bottom: 10),
-                color: myBlue,
-              ),
-              Container(
-                width: double.infinity,
-                height: 49,
-                margin: const EdgeInsets.only(bottom: 10),
-                color: myBlue,
-              ),
-              Container(
-                width: double.infinity,
-                height: 49,
-                margin: const EdgeInsets.only(bottom: 10),
-                color: myBlue,
-              ),
-            ],
-          )
+                child: 
+                Row(
+                  children: [
+                    Container(
+                      width: 50,
+                      alignment: Alignment.center,
+                      child: Text(
+                        index.toString(),
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Gilroy-Bold',
+                          color: myBlue,
+                          ),
+                        ),
+                      ),
+                    Container(height: 30, width: 2, color: Colors.white),
+                    const Expanded (
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 20, right: 15),
+                        child: Text(
+                          'This text  will be in Database Panel. This text  will be in Database Panel. This text  will be in Database Panel.',
+                          style: TextStyle(fontSize: 12,),
+                          maxLines: 2
+                        ),
+                      ),
+                    ),
+                    const Text(
+                      '21.11.2023',
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Color(0xFF9F9F9F),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: Image.asset('assets/img/edit.png'),
+                    ),
+                  ],
+                ),
+              );
+            },
+            ),
+          ),
         ],
       ),
       ));
